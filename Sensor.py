f197efa964ebb2eabc25754749a6355622297589
Chandra Kanth Garalapati <chandrakanthgarlam35674@gmail.com>
2021-11-04 19:21:52 +0530

    Updated Files, Added Flame Sensor Project - Proteus and implemented Server Connection Successfully

--- Sensor.py
+++ Sensor.py
@@ -2,7 +2,10 @@ from flask import Flask, redirect, url_for, request,render_template
 app = Flask(__name__)                                                 # Create Flask Object
 import serial
                                      # Change your IP Address here
-s = serial.Serial('COM2',9600)                                        # Define Serial  Communication Baud rate
+s = serial.Serial('COM2',9600)       # Define Serial  Communication Baud rate
+f = serial.Serial('COM4',9500)
+# p = serial.Serial('COM6',9400)
+
 statusObj = {
    "gas": "NO",
    "flame": "NO",
@@ -59,7 +62,43 @@ def Gas_Sensor():
    except:
       print("Error: unable to fetch data")
 
+@app.route('/Flame_Sensor',methods = ['POST', 'GET'])                   # If light OFF button press then open this servelet 
+def Flame_Sensor():
+   global flag 
+   global statusObj
+   status = request.form['flame_status']
+   try:
+      if(flag==1):                                                   # check valid user
+         if status == "1":
+            f.write(b'a')
+            statusObj["flame"] = "YES"
+         elif status == "0":
+            f.write(b'b')
+            statusObj["flame"] = "NO"                                               # Send 'b' value to proteus software when bulb off detected 
+         return render_template("dashboard.html", statusObj=statusObj) #Refresh the control panel webpage after button press
+      else:
+         return render_template("login.html")  # If user name and password is not correct then after button press then jump to login page 
+   except:
+      print("Error: unable to fetch data")
 
+# @app.route('/Ph_Sensor',methods = ['POST', 'GET'])                   # If light OFF button press then open this servelet 
+# def Ph_Sensor():
+#    global flag 
+#    global statusObj
+#    status = request.form['ph_status']
+#    try:
+#       if(flag==1):                                                   # check valid user
+#          if status == "1":
+#             f.write(b'a')
+#             statusObj["flame"] = "YES"
+#          elif status == "0":
+#             f.write(b'b')
+#             statusObj["flame"] = "NO"                                               # Send 'b' value to proteus software when bulb off detected 
+#          return render_template("dashboard.html", statusObj=statusObj) #Refresh the control panel webpage after button press
+#       else:
+#          return render_template("login.html")  # If user name and password is not correct then after button press then jump to login page 
+#    except:
+#       print("Error: unable to fetch data")
 
 if __name__ == '__main__':
    app.run(port = 5000,debug = False)
