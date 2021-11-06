575f8c18341c0f048b4d6e39c3f0d95e23f42fdc
Chandra Kanth Garalapati <chandrakanthgarlam35674@gmail.com>
2021-11-06 19:53:18 +0530

    IR Sensor Added, PH Sensor Proteus File Added, Fixed issues with Serial Port Communication, Updated Server Side code, Removed the code ( in testing phase of serial port communication from Gas Sensor to Flask Server as Proteus crashes due to memory and CPU Overload error )

--- Sensor.py
+++ Sensor.py
@@ -1,68 +1,84 @@
 from flask import Flask, redirect, url_for, request,render_template
-app = Flask(__name__)                                                 # Create Flask Object
+app = Flask(__name__)      # Create Flask Object
+
+# Other Modules
 import serial
-                                     # Change your IP Address here
-s = serial.Serial('COM2',9600)       # Define Serial  Communication Baud rate
+# import Ports
+# import pio
+                                     
+s = serial.Serial('COM2',9600)       
 f = serial.Serial('COM4',9500)
 # p = serial.Serial('COM6',9400)
+
+i = serial.Serial('COM6',9400)
+
+# pio.uart = Ports.UART()     # Module Object for trying to recieve the data from COM Port
 
 statusObj = {
    "gas": "NO",
    "flame": "NO",
-   "ph": "NO"
+   "ph": "NO",
+   "ir": "Obstacle Not Detected",
+   'button1': 'OFF',
+   'button2' : 'OFF'
 }
 flag = False
 
 @app.route('/') 
 @app.route('/index')                                  # Entry point for web servelet
 def index():
-   return render_template("index.html")  # Jump to home page
+   return render_template("index.html")               # Jump to home page
 
-@app.route('/dashboard')                                                   # Entry point for web servelet
+@app.route('/dashboard')                              # Entry point for dashbaord
 def dashboard():
-   return render_template("dashboard.html", statusObj=statusObj)  # Jump to home page
+   return render_template("dashboard.html", statusObj=statusObj)  # Jump to dashboard page
 
-@app.route('/loginservelate',methods = ['POST', 'GET'])
+@app.route('/loginservelate',methods = ['POST', 'GET'])           # Entry point for web servelet
 def loginservelate():
     global flag
     try:  
-        username = request.form['username']                               # collect ID from webpage 
+        username = request.form['username']                       # collect username from webpage 
         password = request.form['password']                       # collect Password from webpage 
-        if((str(username) == "Ck") and (str(password) == "1234")):   # compare ID and Password
-            flag=1                                                       # if ID and Password is correct then only open bulb control web page
+        if((str(username) == "Ck") and (str(password) == "1234")):# compare username and Password
+            flag=1                                                # if username and Password is correct then set flag to 1
         else:
-            flag=0                                                       # if ID and Password is not correct then open not succesfull webpage
-        if(flag==1):                                                    # if ID and Password is correct then only open bulb control web page
-            return render_template("dashboard.html", statusObj=statusObj) #Open User Home Page webpage
+            flag=0                                                # if username or Password are not correct then set flag to 0
+        
+        if(flag==1):                                              # if flag was 1 open the Dash Board Web page
+            return render_template("dashboard.html", statusObj=statusObj) # Open User Dashbaord Page webpage
         else:
-            return render_template("unsuccessfull.html",name=username)     # Open uncessfull webpage
+            return render_template("unsuccessfull.html",name=username)    # Open unsuccessfull webpage
     except:
         print("Error: unable to fetch data")      
 
-@app.route('/login')                                                   # Entry point for web servelet
+@app.route('/login')                                              # Entry point for login
 def login():
-   return render_template("login.html")  # Jump to login page
+   return render_template("login.html")                           # Jump to login page
 
-@app.route('/Gas_Sensor',methods = ['POST', 'GET'])                   # If light OFF button press then open this servelet 
+@app.route('/Gas_Sensor',methods = ['POST', 'GET'])               # On Submit in Gas Sensor Section open this Servelet
 def Gas_Sensor():
    global flag 
    global statusObj
    status = request.form['gas_status']
    try:
-      if(flag==1):                                                   # check valid user
+      if(flag==1):                                                # check valid user
          if status == "1":
-            s.write(b'a')
-            statusObj["gas"] = "YES"
+            s.write(b'a')                                         # Send 'a' value to proteus software when Gas is passed
+            s.timeout = 10
+            # message = s.read(100)
+            statusObj["gas"] = 'Yes'
          elif status == "0":
-            s.write(b'b')
-            statusObj["gas"] = "NO"                                               # Send 'b' value to proteus software when bulb off detected 
-         return render_template("dashboard.html", statusObj=statusObj) #Refresh the control panel webpage after button press
+            s.write(b'b')                                         # Send 'b' value to proteus software when Gas is not passed
+            s.timeout = 10
+            # message = s.read(100)
+            statusObj["gas"] = 'No'                                          
+         return render_template("dashboard.html", statusObj=statusObj) # Refresh the control panel webpage after button press
       else:
          return render_template("login.html")  # If user name and password is not correct then after button press then jump to login page 
    except:
       print("Error: unable to fetch data")
 
-@app.route('/Flame_Sensor',methods = ['POST', 'GET'])                   # If light OFF button press then open this servelet 
+@app.route('/Flame_Sensor',methods = ['POST', 'GET'])                   # On Submit in Flame Sensor Section open this Servelet
 def Flame_Sensor():
    global flag 
    global statusObj
@@ -70,18 +86,18 @@ def Flame_Sensor():
    try:
       if(flag==1):                                                   # check valid user
          if status == "1":
-            f.write(b'a')
+            f.write(b'a')                                            # Send 'a' value to proteus software when Fire is lit
             statusObj["flame"] = "YES"
          elif status == "0":
-            f.write(b'b')
-            statusObj["flame"] = "NO"                                               # Send 'b' value to proteus software when bulb off detected 
-         return render_template("dashboard.html", statusObj=statusObj) #Refresh the control panel webpage after button press
+            f.write(b'b')                                            # Send 'b' value to proteus software when Fire is put off
+            statusObj["flame"] = "NO"
+         return render_template("dashboard.html", statusObj=statusObj)# Refresh the control panel webpage after button press
       else:
          return render_template("login.html")  # If user name and password is not correct then after button press then jump to login page 
    except:
       print("Error: unable to fetch data")
 
-# @app.route('/Ph_Sensor',methods = ['POST', 'GET'])                   # If light OFF button press then open this servelet 
+# @app.route('/Ph_Sensor',methods = ['POST', 'GET'])                # On Submit in PH Sensor Section open this Servelet  
 # def Ph_Sensor():
 #    global flag 
 #    global statusObj
@@ -89,16 +105,35 @@ def Flame_Sensor():
 #    try:
 #       if(flag==1):                                                   # check valid user
 #          if status == "1":
-#             f.write(b'a')
-#             statusObj["flame"] = "YES"
+#             p.write(b'a')
+#             statusObj["ph"] = "YES"
 #          elif status == "0":
-#             f.write(b'b')
-#             statusObj["flame"] = "NO"                                               # Send 'b' value to proteus software when bulb off detected 
+#             p.write(b'b')
+#             statusObj["ph"] = "NO" 
 #          return render_template("dashboard.html", statusObj=statusObj) #Refresh the control panel webpage after button press
 #       else:
 #          return render_template("login.html")  # If user name and password is not correct then after button press then jump to login page 
 #    except:
 #       print("Error: unable to fetch data")
+
+@app.route('/IR_Sensor',methods = ['POST', 'GET'])                   # On Submit in IR Sensor Section open this Servelet
+def IR_Sensor():
+   global flag 
+   global statusObj
+   status = request.form['ir_status']
+   try:
+      if(flag==1):                                                   # check valid user
+         if status == "1":
+            i.write(b'a')                                            # Send 'a' value to proteus software when Obstacle is Placed
+            statusObj["ir"] = "Obstacle Detected"
+         elif status == "0":
+            i.write(b'b')                                            # Send 'b' value to proteus software when Obstacle is Removed
+            statusObj["ir"] = "Obstacle Not Detected"                
+         return render_template("dashboard.html", statusObj=statusObj) # Refresh the control panel webpage after button press
+      else:
+         return render_template("login.html")  # If user name and password is not correct then after button press then jump to login page 
+   except:
+      print("Error: unable to fetch data")
 
 if __name__ == '__main__':
    app.run(port = 5000,debug = False)
